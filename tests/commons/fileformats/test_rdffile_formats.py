cdeee6781637405cf615d5ca75100cf9a076d0bc
Marc Portier <marc.portier@gmail.com>
2024-07-27 12:45:59 +0200

    move tests to more logic place

--- tests/commons/fileformats/test_rdffile_formats.py
+++ tests/commons/fileformats/test_rdffile_formats.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sema.commons.fileformats import mime_from_filepath
+from sema.commons.fileformats import mime_from_filepath, mime_to_format
 
 
 @pytest.mark.parametrize(
@@ -11,6 +11,17 @@ from sema.commons.fileformats import mime_from_filepath
         (".jsonld", "application/ld+json"),
     ],
 )
-def test_factory_choice(suffix, mime):
+def test_filepath_mime_detection(suffix, mime):
     fname = f"whatever-file-name{suffix}"
     assert mime_from_filepath(fname) == mime
+
+
+def test_mime_to_format():
+    assert mime_to_format("application/ld+json") == "json-ld"
+    assert mime_to_format("text/turtle") == "turtle"
+    assert mime_to_format("application/json") is None
+    assert mime_to_format("application/octet-stream") is None
+    assert mime_to_format("text/html") == "html"
+    assert mime_to_format("application/rdf+xml") == "xml"
+    assert mime_to_format("text/n3") == "n3"
+    assert mime_to_format("application/n-triples") == "nt"
--- tests/discovery/test_url_to_graph.py
+++ tests/discovery/test_url_to_graph.py
@@ -1,31 +1,30 @@
 import logging
-import pytest
 
+import pytest
 from rdflib import Graph
 
-from sema.commons.fileformats import mime_to_format
 from sema.discovery.url_to_graph import get_graph_for_format
 
 log = logging.getLogger(__name__)
 
 
-def test_ctype_to_rdf_format():
-    assert mime_to_format("application/ld+json") == "json-ld"
-    assert mime_to_format("text/turtle") == "turtle"
-    assert mime_to_format("application/json") is None
-    assert mime_to_format("application/octet-stream") is None
-    assert mime_to_format("text/html") == "html"
-    assert mime_to_format("application/rdf+xml") == "xml"
-    assert mime_to_format("text/n3") == "n3"
-    assert mime_to_format("application/n-triples") == "nt"
-
-
-@pytest.mark.parametrize("uri, mime, length", [
-    ("https://www.w3.org/People/Berners-Lee/card.ttl", "text/turtle", 86),
-    ("https://marineregions.org/mrgid/3293.jsonld", "application/ld+json", 99),
-    ("https://data.arms-mbon.org/", "text/turtle", 112),
-    ("https://data.arms-mbon.org/data_release_001/latest/#", "application/json", 532),
-])
+@pytest.mark.parametrize(
+    "uri, mime, length",
+    [
+        ("https://www.w3.org/People/Berners-Lee/card.ttl", "text/turtle", 86),
+        (
+            "https://marineregions.org/mrgid/3293.jsonld",
+            "application/ld+json",
+            99,
+        ),
+        ("https://data.arms-mbon.org/", "text/turtle", 112),
+        (
+            "https://data.arms-mbon.org/data_release_001/latest/#",
+            "application/json",
+            532,
+        ),
+    ],
+)
 def test_discovery_case(uri, mime, length):
     # TODO use the new service in stead
     graph = get_graph_for_format(uri, formats=[mime])
