9022e5cda8f8f5970cab78c45f7101255cd57366
Marc Portier <marc.portier@gmail.com>
2024-07-29 15:27:36 +0200

    expose the logconf_path so it becomes available in tests too

--- sema/commons/log/__init__.py
+++ sema/commons/log/__init__.py
@@ -1,3 +1,4 @@
-from .loader import load_log_config
+from .loader import load_log_config, DEFAULT_LOGGING_CONFIG_LOCATION as logconf_path
 
-__all__ = ["load_log_config"]
+
+__all__ = ["load_log_config", "logconf_path"]
