c66a094a6162d5cb5bf62b34ddbcea53b66b2659
Marc Portier <marc.portier@gmail.com>
2024-08-07 11:37:14 +0200

    lintfix

--- sema/commons/web/httpsession.py
+++ sema/commons/web/httpsession.py
@@ -1,10 +1,15 @@
 from typing import List
+
 from requests import Session
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 
-def make_http_session(total_retry: int = 8, backoff_factor: float = 0.4, status_forcelist: List = [500, 502, 503, 504, 429]) -> Session:
+def make_http_session(
+    total_retry: int = 8,
+    backoff_factor: float = 0.4,
+    status_forcelist: List = [500, 502, 503, 504, 429],
+) -> Session:
     """Create a requests session with retry logic"""
     session = Session()
     retry = Retry(
