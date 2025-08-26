055b844d4e8ef8a7bf9298e06ec84d6033a63f41
bulricht <108867473+bulricht@users.noreply.github.com>
2025-08-26 09:56:45 +0200

    refactor

--- sema/commons/rdf_ogm/__init__.py
+++
@@ -1,3 +0,0 @@
-from .graph_builder import GraphBuilder
-from .term_builder import TermBuilder
-from .object_graph_mapper import ObjectGraphMapper
--- sema/commons/rdf_ogm/graph_builder.py
+++
@@ -1,7 +0,0 @@
-class GraphBuilder:
-    def __init__(self):
-        self. graph = ...
-        self._build()
-    
-    def _build(self):
-        raise NotImplementedError
