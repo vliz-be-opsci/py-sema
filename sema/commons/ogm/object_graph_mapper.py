effc1e57e3f66541249500a60253ff5a50d0f5b7
bulricht <108867473+bulricht@users.noreply.github.com>
2025-10-08 16:50:59 +0200

    implement sorted jsonld output

--- sema/commons/ogm/object_graph_mapper.py
+++ sema/commons/ogm/object_graph_mapper.py
@@ -1,3 +1,4 @@
+import json
 from rdflib import Graph
 from abc import ABC, abstractmethod
 
@@ -22,22 +23,36 @@ class ObjectGraphMapper(ABC):
 
     def __str__(self, destination=None, format=None, *args, **kwargs) -> str:
         if format == "application/ld+json":
-            return self.graph.serialize(
-                destination=destination,
-                format=format,
-                context=self.graph._jsonld_context_repr if hasattr(self.graph, "_jsonld_context_repr") else None,
-                auto_compact=True,
-                indent=4,
-                *args,
-                **kwargs
-            )
+            return self.__application_ld_json__(destination=destination, *args, **kwargs)
+        
+        return self.__text_turtle__(destination=destination, *args, **kwargs)
 
+    
+    def __text_turtle__(self, destination=None, *args, **kwargs) -> str:
         return self.graph.serialize(
             destination=destination,
-            format=format or "text/turtle",
+            format="text/turtle",
             *args,
             **kwargs
         )
+
+    def __application_ld_json__(self, destination=None, *args, **kwargs) -> str:
+        indent = kwargs.get("indent", 4)
+        data = json.loads(self.graph.serialize(
+            format="application/ld+json",
+            context=self.graph._jsonld_context_repr if hasattr(self.graph, "_jsonld_context_repr") else None,
+            auto_compact=True,
+            indent=indent,
+            *args,
+            **kwargs
+        ))
+
+        data["@graph"] = sorted(data["@graph"], key=lambda node: node.get("@id"))
+
+        if destination:
+            json.dump(data, open(destination, "w", encoding="utf-8"), indent=indent)
+
+        return json.dumps(data, indent=indent)
 
     @abstractmethod
     def _map(self) -> Graph:
