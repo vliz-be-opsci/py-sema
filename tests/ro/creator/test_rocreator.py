20f9429251b9a498463674bcd708f3a0f903eb06
bulricht <108867473+bulricht@users.noreply.github.com>
2026-01-13 14:24:37 +0100

    cleanup roc test dir

--- tests/ro/creator/test_rocreator.py
+++ tests/ro/creator/test_rocreator.py
@@ -2,7 +2,7 @@ from sema.ro.creator import ROCreator
 
 def test_object_graph_mapper():
     roc = ROCreator(
-        blueprint_path="./tests/ro/creator/data/katoomba-rainfall/roc-me.yml",
+        blueprint_path="./tests/ro/creator/data/katoomba-rainfall/sema_roc.yml",
         blueprint_env={"SESSIONNAME": "ApiConsole"},
         rocrate_path="./tests/ro/creator/data/katoomba-rainfall",
     )
