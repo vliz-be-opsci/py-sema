0660e3cb72af1a8d6b06285ec1a6064753d81032
bulricht <108867473+bulricht@users.noreply.github.com>
2026-01-13 13:45:53 +0100

    add ogm test

--- tests/commons/ogm/test_object_graph_mapper.py
+++ tests/commons/ogm/test_object_graph_mapper.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+from rdflib import Graph
+from rdflib.compare import to_isomorphic
 from sema.commons.ogm import ObjectGraphMapper, GraphBuilder
 
 def test_object_graph_mapper():
     class OGMLike(ObjectGraphMapper):
-        def _map(self):
-            return GraphBuilder().build()
+        def __init__(self, blueprint):
+            super().__init__()
+            self.blueprint = Path(blueprint)
 
-    OGMLike().serialize()
+        def _map(self):
+            return GraphBuilder(
+                namespaces={"@base": "http://example.org/"}, # overriding default value to circumvent rdflib bug (https://github.com/RDFLib/rdflib/issues/1216)
+                blueprint=self.blueprint
+            ).build()
+
+    ogml = OGMLike("./tests/commons/ogm/data/object_graph_mapping.yml")
+    ogml.serialize("./tests/commons/ogm/data/object_graph_mapping.ttl")
+
+    g0 = Graph().parse("./tests/commons/ogm/data/object_graph_mapping_expected.ttl")
+    g1 = Graph().parse("./tests/commons/ogm/data/object_graph_mapping.ttl")
+
+    assert to_isomorphic(g0) == to_isomorphic(g1)
