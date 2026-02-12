cb4fd8119d9b1e952a03cc7e8c552bf9b0e97512
bulricht <108867473+bulricht@users.noreply.github.com>
2026-02-12 15:23:06 +0100

    implement commons.aggregator

---
+++ sema/commons/aggregator/aggregator.py
@@ -0,0 +1,32 @@
+from rdflib import Graph
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Aggregator:
+    def __init__(
+            self,
+            input_path: str | Path,
+            globs: str,
+            output_path: str | Path | None = None,
+            output_format: str | None = None
+        ) -> None:
+        self.input_path = Path(input_path)
+        self.output_path = Path(
+            output_path or self.input_path / "graph.ttl"
+        )  # output path should include the file name
+        self.output_format = output_format
+        self.globs = {k.strip(): v.strip() for k, v in (i.strip().split(":") for i in globs.split(","))}
+        self.graph = Graph()
+
+    def aggregate(self) -> None:
+        for glb, fmt in self.globs.items():
+            for p in self.input_path.rglob(glb):
+                if p.is_file():
+                    try:
+                        self.graph.parse(p, format=fmt)
+                    except Exception as e:
+                        logger.error(f"failed to parse {p}: {e}")
+
+        self.graph.serialize(self.output_path, format=self.output_format)
---
+++ tests/commons/aggregator/test_aggregator.py
@@ -0,0 +1,5 @@
+def test_aggregator():
+    """
+    globs = sediment/**/*: ttl, water/**/*: ttl, ro-crate-metadata.json: json-ld
+    """
+    ...
