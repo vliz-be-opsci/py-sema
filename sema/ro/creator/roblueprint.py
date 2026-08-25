3d2b17e727e7b86ee5721c09e5633afddfc06d15
bulricht <108867473+bulricht@users.noreply.github.com>
2026-08-25 23:00:42 +0200

    implement glob sorting

--- sema/ro/creator/roblueprint.py
+++ sema/ro/creator/roblueprint.py
@@ -1,4 +1,8 @@
 from copy import deepcopy
+from functools import cache
+from itertools import product
+
+import networkx as nx
 
 from sema.commons.glob import getMatchingGlobPaths
 from sema.commons.ogm import GraphBlueprint
@@ -13,9 +17,8 @@ class ROBlueprint(GraphBlueprint):
         version="latest",
         nested_datasets=False,
         glob_root=None,
-        glob_rnode=True,
         glob_walk=False,
-        glob_ignore: list[str] | str = [],
+        glob_ignore: list[str] | str | None = None,
         *args,
         **kwargs,
     ):
@@ -36,45 +39,63 @@ class ROBlueprint(GraphBlueprint):
         )
 
         self.nested_datasets = nested_datasets
-        self.implicit_body = {k: v for k, v in self.body.items() if "*" in k}
-        self.explicit_body = {
-            k: v for k, v in self.body.items() if not ("*" in k)
-        }
         self.glob_root = glob_root
-        self.glob_ignore = glob_ignore
-        if glob_walk:
-            self.implicit_body["**/*"] = {}
-        if glob_walk or glob_rnode:
-            self._expand_glob()
-
+        self.glob_ignore = glob_ignore or []
+        if glob_walk and (not "**/*" in self.body):
+            self.body["**/*"] = {}
+        self._expand_body()
         assert (
             not self.nested_datasets
         ), "nested datasets are not yet supported"  # TODO support nested datasets # noqa: E501
 
-    def _expand_glob(self):
-        assert self.glob_root, "glob_root must be set to expand glob patterns"
-        self.body = {}
-        for glob_pattern, property in self.implicit_body.items():
-            paths = getMatchingGlobPaths(
+    @cache
+    def _expand_glob(self, glob):
+        fs = frozenset(
+            getMatchingGlobPaths(
                 root=self.glob_root,
-                includes=glob_pattern,
+                includes=glob,
                 excludes=self.glob_ignore,
             )
-            for path in paths:
-                if (self.glob_root / path).is_file():
-                    if not property.get("$type"):
-                        property["$type"] = "File"
-                    self.body[str(path)] = deepcopy(property)
-                else:
-                    if not property.get("$type"):
-                        property["$type"] = "Dataset"
+        )
 
-        for identifier, property in self.explicit_body.items():
-            if identifier in self.body:
-                self.body[identifier].update(property)
-            else:
-                self.body[identifier] = property
+        if len(fs) == 0:
+            raise ValueError(f"glob pattern {glob} did not match any files")
+
+        return fs
+
+    def _expand_body(self):
+        assert self.glob_root, "glob_root must be set to expand glob patterns"
+        globs = [k for k in self.body.keys() if not (k == "./" or "://" in k)]
+
+        # create a DAG of globs, pointing from more general to more specific
+        dg = nx.DiGraph()
+        dg.add_nodes_from(globs)
+        dg.add_edges_from(
+            [
+                (x, y)
+                for x, y in product(globs, globs)
+                if x != y
+                and self._expand_glob(y).issubset(self._expand_glob(x))
+            ]
+        )
+
+        # flatten the DAG into a list of file paths,
+        # sorted from more general to more specific
+        expanded_body = {}
+        for glob in nx.topological_sort(dg):
+            paths = self._expand_glob(glob)
+            for path in paths:
+                p = path.as_posix()
+                if not p in expanded_body:
+                    expanded_body[path.as_posix()] = deepcopy(self.body[glob])
+                else:
+                    expanded_body[path.as_posix()].update(self.body[glob])
+
+        # handle rocrate root and URIs
+        for k, v in self.body.items():
+            if k == "./" or "://" in k:
+                expanded_body[k] = v
 
         self.body = dict(
-            sorted(self.body.items())
+            sorted(expanded_body.items())
         )  # sort keys to make output deterministic
